08b99f2f756490ea07614611488dc5bc480f4782
Erwin de Vreugd <erwindevreugd@casema.nl>
2017-10-19 12:47:28 +0200

    Fixed parameter issue with creating a new badge

--- New-Badge.ps1
+++ New-Badge.ps1
@@ -13,23 +13,36 @@ function New-Badge
         [int]$PersonID,
 
 		[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
-        [long]$BadgeID
+        [long]$BadgeID,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [long]$BadgeTypeID
     )
 
-    DynamicParam {
-        $PrinterNameAttribute = New-Object System.Management.Automation.ParameterAttribute
-        $PrinterNameAttribute.Mandatory = $true
+    # DynamicParam {
+    #     # Set the dynamic parameters' name
+    #     $ParamName_BadgeType = 'BadgeType'
+    #     # Create the collection of attributes
+    #     $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+    #     # Create and set the parameters' attributes
+    #     $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+    #     $ParameterAttribute.Mandatory = $true
+    #     $ParameterAttribute.Position = 4
+    #     # Add the attributes to the attributes collection
+    #     $AttributeCollection.Add($ParameterAttribute) 
+    #     # Create the dictionary 
+    #     $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+    #     # Generate and set the ValidateSet 
+    #     $arrSet = (Get-BadgeType).Name
+    #     $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)    
+    #     # Add the ValidateSet to the attributes collection
+    #     $AttributeCollection.Add($ValidateSetAttribute)
+    #     # Create and return the dynamic parameter
+    #     $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParamName_BadgeType, [string], $AttributeCollection)
+    #     $RuntimeParameterDictionary.Add($ParamName_BadgeType, $RuntimeParameter)
 
-        $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
-        $attributeCollection.Add($PrinterNameAttribute)
-
-        $PrinterNameParameter = New-Object System.Management.Automation.RuntimeDefinedParameter(
-            'BadgeType', [string], $attributeCollection
-        )
-        $paramDictionary = new-object System.Management.Automation.RuntimeDefinedParameterDictionary
-        $paramDictionary.Add('BadgeType', $PrinterNameParameter)
-        return $paramDictionary
-    }
+    #     return $RuntimeParameterDictionary
+    # }
 
     process {
         $parameters = @{
@@ -47,9 +60,9 @@ function New-Badge
 		}
 
 		Set-WmiInstance @parameters -Arguments @{
-			ID=$ID;
+			ID=$BadgeID;
             PERSONID=$PersonID;
-            TYPE=$Type;
+            TYPE=$BadgeTypeID;
             STATUS=1} |
 			Select-Object *,@{L='BadgeID';E={$_.ID}} | 
 			Get-Badge
