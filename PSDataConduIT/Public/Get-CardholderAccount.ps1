c8c306b9bbb9a9e48dd5d7d808b7e64ef37eacb7
Erwin de Vreugd <erwindevreugd@casema.nl>
2018-12-07 06:17:36 +0100

    Added Get-CardholderAccount

---
+++ PSDataConduIT/Public/Get-CardholderAccount.ps1
@@ -0,0 +1,53 @@
+<#
+    .SYNOPSIS
+    Gets a cardholders account.
+
+    .DESCRIPTION   
+    Gets all cardholder accounts or a single cardholders account if a cardholder id is specified. 
+    
+    If the result return null, try the parameter "-Verbose" to get more details.
+    
+    .EXAMPLE
+    
+    .LINK
+    https://github.com/erwindevreugd/PSDataConduIT
+#>
+function Get-CardholderAccount
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(
+            Position=0, 
+            Mandatory=$false, 
+            ValueFromPipelineByPropertyName=$true,
+            HelpMessage='The name of the server where the DataConduIT service is running or localhost.')]
+        [string]$Server = $Script:Server,
+        
+        [Parameter(
+            Position=1,
+            Mandatory=$false, 
+            ValueFromPipelineByPropertyName=$true,
+            HelpMessage='The credentials used to authenticate the user to the DataConduIT service.')]
+        [PSCredential]$Credential = $Script:Credential,
+
+        [Parameter(
+            Mandatory=$false, 
+            ValueFromPipelineByPropertyName=$true,
+            HelpMessage='The cardholder id parameter.')]
+        [int]$CardholderID
+    )
+
+    process {
+        $parameters = @{
+            Server=$Server;
+            PersonID=$CardholderID
+        }
+
+        if($Credential -ne $null) {
+            $parameters.Add("Credential", $Credential)
+        }
+
+        Get-Account @parameters
+    }
+}
