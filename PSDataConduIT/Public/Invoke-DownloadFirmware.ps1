ebe480a9885c03157d6fbfad8304d2c1be36e2b4
Erwin de Vreugd <erwindevreugd@casema.nl>
2018-12-10 05:45:54 +0100

    Closes #46

--- PSDataConduIT/Public/Invoke-DownloadFirmware.ps1
+++ PSDataConduIT/Public/Invoke-DownloadFirmware.ps1
@@ -15,7 +15,7 @@
 function Invoke-DownloadFirmware {
     [Alias("Download-Firmware")]
     [CmdletBinding(
-        DefaultParameterSetName = 'DownloadFirmwareToPanel',
+        DefaultParameterSetName = 'DownloadFirmwareToAll',
         SupportsShouldProcess,
         ConfirmImpact = "High"
     )]
@@ -75,11 +75,16 @@ function Invoke-DownloadFirmware {
             $parameters.Add("Credential", $Credential)
         }
 
-        switch ($PSCmdlet.ParameterSetName) {
-            "DownloadFirmwareToPanel" {
-                if (($panel = Get-Panel @parameters -PanelID $PanelID) -eq $null) {
-                    Write-Error -Message ("Panel id '$($PanelID)' not found")
-                    return
+        function DownloadFirmwareToPanel($PanelID) {
+            if (($panels = Get-Panel @parameters -PanelID $PanelID) -eq $null) {
+                Write-Error -Message ("Panel id '$($PanelID)' not found")
+                return
+            }
+
+            foreach ($panel in $panels) {
+                if ($panel.Type -eq "Logical Source") {
+                    Write-Verbose -Message ("Download firmware is not possible on panel '$($panel.Name)' of type '$($panel.Type)', skipping.")
+                    continue
                 }
 
                 if ($Force -or $PSCmdlet.ShouldProcess("$Server", "Download firmware to panel '$($panel.Name)'")) {
@@ -87,16 +92,35 @@ function Invoke-DownloadFirmware {
                     Write-Verbose -Message ("Downloaded firmware to panel '$($panel.Name)'")
                 }
             }
-            "DownloadFirmwareToReader" {
-                if (($reader = Get-Reader @parameters -PanelID $PanelID -ReaderID $ReaderID) -eq $null) {
-                    Write-Error -Message ("Reader id '$($ReaderID)' on panel id '$($PanelID)' not found")
-                    return
-                }
+        }
 
+        function DownloadFirmwareToReader($PanelID, $ReaderID) {
+            if (($readers = Get-Reader @parameters -PanelID $PanelID -ReaderID $ReaderID) -eq $null) {
+                Write-Error -Message ("Reader id '$($ReaderID)' on panel id '$($PanelID)' not found")
+                return
+            }
+
+            foreach ($reader in $readers) {
                 if ($Force -or $PSCmdlet.ShouldProcess("$Server", "Download firmware to reader '$($reader.Name)'")) {
                     $reader.DownloadFirmware.Invoke() | Out-Null
                     Write-Verbose -Message ("Downloaded firmware to reader '$($reader.Name)'")
                 }
+            }
+        }
+
+        switch ($PSCmdlet.ParameterSetName) {
+            "DownloadFirmwareToPanel" {
+                Write-Verbose -Message ("Download firmware to panel(s)")
+                DownloadFirmwareToPanel $PanelID
+            }
+            "DownloadFirmwareToReader" {
+                Write-Verbose -Message ("Download firmware to reader(s)")
+                DownloadFirmwareToReader $PanelID $ReaderID
+            }
+            "DownloadFirmwareToAll" {
+                Write-Verbose -Message ("Download firmware to panel(s) and reader(s)")
+                DownloadFirmwareToPanel $PanelID
+                DownloadFirmwareToReader $PanelID $ReaderID
             }
             Default { return }
         }
