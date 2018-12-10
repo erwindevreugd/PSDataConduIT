8967ee689b2aa383deeea6a791b12ade25acde01
Erwin de Vreugd <erwindevreugd@casema.nl>
2018-12-10 08:15:20 +0100

    Added Set-User

---
+++ PSDataConduIT/Public/Set-User.ps1
@@ -0,0 +1,183 @@
+<#
+    .SYNOPSIS
+    Updates a user.
+
+    .DESCRIPTION
+    Updates a user in the database.
+
+    If the result returns null, try the parameter "-Verbose" to get more details.
+
+    .EXAMPLE
+
+    .LINK
+    https://github.com/erwindevreugd/PSDataConduIT
+#>
+function Set-User {
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        "PSAvoidUsingPlainTextForPassword",
+        "",
+        Justification="OnGuard requires this to be plain text")]
+    param
+    (
+        [Parameter(
+            Position = 0,
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The name of the server where the DataConduIT service is running or localhost.')]
+        [string]
+        $Server = $Script:Server,
+
+        [Parameter(
+            Position = 1,
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The credentials used to authenticate the user to the DataConduIT service.')]
+        [PSCredential]
+        $Credential = $Script:Credential,
+
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The user id parameter.')]
+        [int]
+        $UserID,
+
+        [ValidateLength(1, 255)]
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The firsname name of the user.')]
+        [string]
+        $Firstname,
+
+        [ValidateLength(1, 255)]
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The firsname name of the user.')]
+        [string]
+        $Lastname,
+
+        [ValidateLength(1, 255)]
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The internal username of the user.')]
+        [string]
+        $LogonID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The password for the user.')]
+        [string]
+        $Password,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The system permission group id of the user.')]
+        [int]
+        $SystemPermissionGroupID = -1,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The monitoring permission group id of the user.')]
+        [int]
+        $MonitoringPermissionGroupID = -1,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The card permission group id of the user.')]
+        [int]
+        $CardPermissionGroupID = -1,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The field permission id of the user.')]
+        [int]
+        $FieldPermissionID = -1,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The report permission group id of the user.')]
+        [int]
+        $ReportPermissionGroupID = -1,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The monitoring zone id of the user.')]
+        [int]
+        $MonitoringZoneID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The primary segment id of the user.')]
+        [int]
+        $SegmentID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Determines wheter the new user is enabled or disabled.')]
+        [bool]
+        $Enabled = $true,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'The note field for the user.')]
+        [string]
+        $Notes
+    )
+
+    process {
+        $query = "SELECT * FROM Lnl_User WHERE __CLASS='Lnl_User'"
+
+        if ($UserID) {
+            $query += " AND ID=$UserID"
+        }
+
+        LogQuery $query
+
+        $parameters = @{
+            ComputerName = $Server;
+            Namespace    = $Script:OnGuardNamespace;
+            Query        = $query
+        }
+
+        if ($Credential -ne $null) {
+            $parameters.Add("Credential", $Credential)
+        }
+
+        if (($user = Get-WmiObject @parameters) -eq $null) {
+            Write-Error -Message ("User id '$($UserID)' not found")
+            return
+        }
+
+        Set-WmiInstance -InputObject $user -Arguments @{
+            FirstName                   = $Firstname;
+            LastName                    = $Lastname;
+            LogonID                     = $LogonID;
+            Password                    = $Password;
+            SystemPermissionGroupID     = $SystemPermissionGroupID;
+            MonitoringPermissionGroupID = $MonitoringPermissionGroupID;
+            CardPermissionGroupID       = $CardPermissionGroupID;
+            FieldPermissionID           = $FieldPermissionID;
+            ReportPermissionGroupID     = $ReportPermissionGroupID;
+            MonitoringZoneID            = $MonitoringZoneID;
+            PrimarySegmentID            = $SegmentID;
+            Enabled                     = $Enabled;
+            Notes                       = $Notes;
+        } -PutType UpdateOnly |
+            Select-Object *, @{L = 'UserID'; E = {$_.ID}} |
+            Get-User
+    }
+}
