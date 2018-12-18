6afc397bc83875c3215b249d6b96bcc1c93f6a27
Erwin de Vreugd <erwindevreugd@casema.nl>
2018-12-18 13:24:31 +0100

    Closes #67

--- PSDataConduIT/Public/Get-LoggedEvent.ps1
+++ PSDataConduIT/Public/Get-LoggedEvent.ps1
@@ -43,6 +43,91 @@ function Get-LoggedEvent {
         [Parameter(
             Mandatory = $false,
             ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the description of the logged event(s) to get. Wildcard are permitted.')]
+        [string]
+        $Description,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the event text of the logged event(s) to get. Wildcard are permitted.')]
+        [string]
+        $EventText,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the badge id of the logged event(s) to get.')]
+        [long]
+        $BadgeID = $null,
+
+        [Alias("CardholderID")]
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the person id of the logged event(s) to get.')]
+        [int]
+        $PersonID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the panel id of the logged event(s) to get.')]
+        [int]
+        $PanelID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the device id of the logged event(s) to get.')]
+        [int]
+        $DeviceID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the secondary device id of the logged event(s) to get.')]
+        [int]
+        $SecondaryDeviceID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the extended id of the logged event(s) to get.')]
+        [int]
+        $ExtendedID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the asset id of the logged event(s) to get.')]
+        [int]
+        $AssetID,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies the type of access result of the logged event(s) to get.')]
+        [accessresult]
+        $AccessResult,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies wheter or not the duress field was set of the logged event(s) to get.')]
+        [nullable[bool]]
+        $Duress,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Specifies wheter or not the cardholder entered field was set of the logged event(s) to get.')]
+        [nullable[bool]]
+        $CardholderEntered,
+
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true,
             HelpMessage = 'Specifies the segment id of the logged event(s) to get.')]
         [int]
         $SegmentID = 0
@@ -53,6 +138,54 @@ function Get-LoggedEvent {
 
         if ($SerialNumber) {
             $query += " AND SERIALNUMBER=$SerialNumber"
+        }
+
+        if ($Description) {
+            $query += " AND DESCRIPTION like '$(ToWmiWildcard $Description)'"
+        }
+
+        if ($EventText) {
+            $query += " AND EVENTTEXT like '$(ToWmiWildcard $EventText)'"
+        }
+
+        if ($BadgeID) {
+            $query += " AND CARDNUMBER=$BadgeID"
+        }
+
+        if ($PersonID) {
+            $query += " AND PERSONID=$PersonID"
+        }
+
+        if ($PanelID) {
+            $query += " AND PANELID=$PanelID"
+        }
+
+        if ($DeviceID) {
+            $query += " AND DEVICEID=$DeviceID"
+        }
+
+        if ($SecondaryDeviceID) {
+            $query += " AND SECONDARYDEVICEID=$SecondaryDeviceID"
+        }
+
+        if ($ExtendedID) {
+            $query += " AND EXTENDEDID=$ExtendedID"
+        }
+
+        if ($AssetID) {
+            $query += " AND ASSETID=$AssetID"
+        }
+
+        if ($AccessResult) {
+            $query += " AND ACCESSRESULT=$([int]$AccessResult)"
+        }
+
+        if ($Duress -or $Duress -eq $false) {
+            $query += " AND DURESS=$($Duress)"
+        }
+
+        if ($CardholderEntered -or $CardholderEntered -eq $false) {
+            $query += " AND CARDHOLDERENTERED=$($CardholderEntered)"
         }
 
         if ($SegmentID -ne 0) {
